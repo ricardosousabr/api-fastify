59df65b41f11e7155d01c393f7f405fce2aae0ef
ricardosousabr <ricardo.br.pi@gmail.com>
2024-10-17 23:14:11 -0300

    feat: add env in project e dontenv

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const app = express()
-const port = 3000
+require('dotenv').config()
+
+app.use((req, res, next) => {
+  console.log("Data de hoje", Date.now())
+  next()
+})
 
 app.get('/', (req, res) => {
   res.send('hello Ricardo')
 })
 
-app.listen(port, () => {
-  console.log(`Rordando na porta ${port}`)
-})
+
+app.listen(process.env.PORT)
