b1079732a07310fe45d7720084223b3e08263cf4
ricardosousabr <ricardo.br.pi@gmail.com>
2024-10-28 20:37:35 -0300

    perf: remove bodyparser

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ const express = require('express')
 const app = express()
 require('dotenv').config()
 const routerMain = require('./routers/picking-tasks')
+const bodyParser = require('body-parser')
+app.use(bodyParser.json());
 
 app.use('/tasks', routerMain)
 
+// eslint-disable-next-line no-undef
 app.listen(process.env.PORT)
