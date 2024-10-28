a47c5aed0521d8769af564aba90604beeb843899
ricardosousabr <ricardo.br.pi@gmail.com>
2024-10-28 20:38:58 -0300

    feat: create update request and single task request

--- src/controllers/todoController.js
+++ src/controllers/todoController.js
@@ -1,32 +1,55 @@
 let task = [
   {
     id: 1,
-    name: "Fazer compras",
+    name: "Fazer compras"
   },
   {
     id: 2,
-    name: "Ler um livro",
-  },
+    name: "Ler um livro"
+  }
 ]
 
-const allTasks = (req, res) => {
+const allTasks = (_, res) => {
   res.json(task);
 }
 
+const taskByID = (req, res) => {
+  let { id } = req.params;
+  if(!task[id-1]) return res.status(404).json({msg: 'Task not found'});
+  res.json(task[id-1])
+}
+
 const createTask = (req, res) => {
-  console.log(req.body);
-  const newId = task.length++;
-  const { name } = req.body;
-  const newTask = { id: newId, name };
-  task[newId] = newTask
+  const newTask = {
+    id: task.length + 1,
+    name: req.body.name
+  };
+  if(newTask.id) return res.status(200).json({msg: 'Task created'});
+  task.push(newTask);
   res.json(newTask);
+};
+
+const updateTask = (req, res) => {
+  let {id} = req.params
+  let update = {
+    id: id,
+    name: req.body.name
+  }
+  if(!id) return res.status(200).json({msg: 'Task not found'})
+  task[id-1] = update
+  res.json(update)
 }
 
 const deleteTask = (req, res) => {
-  const { id } = req.params
+  let { id } = req.params
+  if(!id) return res.status(200).json({msg: 'Task not found'})
   let filterTask = task.filter(todo => todo.id != id)
   task = filterTask
-  res.json({ message: 'To-do removido com sucesso' });
+  res.json({ message: 'To-do removido com sucesso' })
 }
 
-module.exports = {allTasks, deleteTask, createTask}
+module.exports = {allTasks, taskByID, createTask, updateTask, deleteTask}
+
+//Criar a rota put
+// adicionar as validações
+// Adicionar um banco de dados  e fazer a conexãoS
