2d8480296bd0ab3d11a9582a812a829bec1c6efb
ricardosousabr <ricardo.br.pi@gmail.com>
2024-11-15 23:37:27 -0300

    feat: create validation create e update user

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,11 +1,23 @@
 const db = require('../conection/conection')
 const { ObjectId } = require('mongodb');
 
+const getUser = async (req, res) => {
+  let {user} = req.body
+  let collection = db.collection('user');
+  let result = await collection.findOne({userName: user});
+  res.status(200).json(result)
+}
+
 const createUser = (req, res) => {
   let {user, pass} = req.body;
   let collection = db.collection('user');
-  let result = collection.insertOne({userName: user, password: pass});
-  res.status(200).json({msg: 'User created successfully'});
+
+  if (user != null && user != "" && pass != null && pass != "" ) {
+    let result = collection.insertOne({userName: user, password: pass});
+    res.status(200).json({msg: 'User created successfully'});
+  } else{
+    res.status(500).json({msg: 'the user cannot be null or empty'});
+  }
 }
 
 const updateUser = async (req, res) => {
@@ -13,6 +25,15 @@ const updateUser = async (req, res) => {
   let {id} = req.params
   let name = req.body
   let collection = await db.collection("user")
+
+
+  if (id != null && id != "" && name != null && name != "" ) {
+    let result = collection.updateOne({_id: new ObjectId(id)}, {$set: {userName: name}})
+    res.status(200).json({msg: 'User aupdate successfully'})
+    } else{
+    res.status(500).json({msg: 'the user cannot be null or empty'});
+  }
+
   let result = collection.updateOne({_id: new ObjectId(id)}, {$set: {userName: name}})
   res.status(200).json({msg: 'User aupdate successfully'})
 }
@@ -24,4 +45,4 @@ const deleteUser = async (req, res) => {
   res.status(200).json({msg: 'User deleted'})
 }
 
-module.exports = {createUser, updateUser, deleteUser};
+module.exports = {getUser, createUser, updateUser, deleteUser};
