4b381a4fd7a35e55a27f058febe872f7460c9bfd
ricardosousabr <ricardo.br.pi@gmail.com>
2024-11-26 23:22:50 -0300

    perf: remove the old template from the project

--- client/src/components/card/index.tsx
+++
@@ -1,8 +0,0 @@
-export default function Card(){
-  return (
-    <>
-      <h2>Enter your account</h2>
-      <div></div>
-    </>
-  )
-}
--- server/src/conection/conection.js
+++
@@ -1,6 +0,0 @@
-const {MongoClient}  = require("mongodb");
-const connectionString = process.env.MONGO_URI || "";
-const client = new MongoClient(connectionString);
-let db = client.db("taskUser");
-
-module.exports = db;
