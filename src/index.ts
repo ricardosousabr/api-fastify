930535c8f437e96880f520f7712d191f7d97a351
ricardosousabr <ricardo.br.pi@gmail.com>
2024-12-07 19:33:45 -0300

    feat: create router delete, put and configuration host

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import Fastify, { FastifyInstance } from 'fastify'
 import dotenv from 'dotenv'
+import createUser from './routes/user/create'
 import {
   serializerCompiler,
   validatorCompiler,
@@ -12,31 +13,32 @@ const fastify: FastifyInstance = Fastify({
   logger: true,
 })
 
+fastify.register(createUser)
+
 fastify.setValidatorCompiler(validatorCompiler)
 fastify.setSerializerCompiler(serializerCompiler)
 
-fastify.withTypeProvider<ZodTypeProvider>().route({
-  method: 'POST',
-  url: '/login',
-  schema: {
-    body: z
-      .object({
-        userName: z.string().min(6).max(10),
-      })
-      .required(),
-    response: {
-      200: z.string(),
-    },
-  },
-  handler: (req, res) => {
-    res.send(req.body.userName)
-  },
-})
+// fastify.withTypeProvider<ZodTypeProvider>().route({
+//   method: 'POST',
+//   url: '/login',
+//   schema: {
+//     body: z
+//       .object({
+//         userName: z.string().min(6).max(10),
+//       })
+//       .required(),
+//     response: {
+//       200: z.string(),
+//     },
+//   },
+//   handler: (req, res) => {
+//     res.send(req.body.userName)
+//   },
+// })
 
 fastify.withTypeProvider<ZodTypeProvider>().route({
   method: 'GET',
   url: '/user/:name',
-  // Define your schema
   schema: {
     params: z.object({
       name: z.string().min(4),
@@ -50,8 +52,62 @@ fastify.withTypeProvider<ZodTypeProvider>().route({
   },
 })
 
+fastify.withTypeProvider<ZodTypeProvider>().route({
+  method: 'PUT',
+  url: '/user/:id',
+  schema: {
+    params: z.object({
+      id: z.string().min(5).max(15),
+    }),
+    response: {
+      200: z.string(),
+    },
+  },
+  handler: (req, res) => {
+    const newName = 'Maria'
+    const user = {
+      id: '12345',
+      name: 'Ricardo',
+    }
+
+    user.name = newName
+    res.send(`User with ID: ${user.name} updated`)
+  },
+})
+
+fastify.withTypeProvider<ZodTypeProvider>().route({
+  method: 'DELETE',
+  url: '/user/:id',
+  schema: {
+    params: z.object({
+      id: z.string().min(5).max(15),
+    }),
+    response: {
+      200: z.string(),
+    },
+  },
+  handler: (req, res) => {
+    const user = [
+      {
+        id: '12345',
+        name: 'Ricardo',
+      },
+      {
+        id: '67890',
+        name: 'Maria',
+      },
+    ]
+
+    user.splice(
+      user.findIndex((u) => u.id === req.params.id),
+      1
+    )
+    res.send(`User with ID: ${user} deleted`)
+  },
+})
+
 try {
-  await fastify.listen({ port: Number(process.env.PORT) })
+  await fastify.listen({ port: Number(process.env.PORT), host: '0.0.0.0' })
 } catch (err) {
   fastify.log.error(err)
   process.exit(1)
