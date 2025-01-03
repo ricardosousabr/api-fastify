17d0b226971ffc1f9a83a94987330ae447582128
ricardosousabr <ricardo.br.pi@gmail.com>
2025-01-02 23:09:35 -0300

    feat: update test

--- src/routes/user/tests/create.test.ts
+++ src/routes/user/tests/create.test.ts
@@ -15,35 +15,60 @@ describe('User Route', () => {
     await app.close()
   })
 
-  it('should return the name if it is valid', async () => {
-    const response = await supertest(app.server).get('/user?name=John')
+  it('should return 400 if the name is too short', async () => {
+    const user = {
+      name: 'jay',
+      password: '12345678',
+    }
+    const response = await supertest(app.server).post('/user').send(user)
 
-    expect(response.status).toBe(200)
-    expect(response.text).toBe('John')
+    expect(response.status).toBe(400)
   })
 
-  it('should return 400 if the name is too short', async () => {
-    const response = await supertest(app.server).get('/user?name=Jo')
+  it('should return 400 if the password is too short', async () => {
+    const user = {
+      name: 'Ricardo',
+      password: '123',
+    }
+    const response = await supertest(app.server).post('/user').send(user)
 
     expect(response.status).toBe(400)
   })
 
   it('should return 400 if the name is too long', async () => {
-    const response = await supertest(app.server).get(
-      '/user?name=ThisNameIsWayTooLong'
-    )
+    const user = {
+      name: 'Verylongusername',
+      password: '12345678',
+    }
+
+    const response = await supertest(app.server).post('/user').send(user)
 
     expect(response.status).toBe(400)
   })
 
-  it('should return 400 if the name contains invalid characters', async () => {
-    const response = await supertest(app.server).get('/user?name=John123')
+  it('should return 400 if the password is too long', async () => {
+    const user = {
+      name: 'Ricardo',
+      password: 'Verylonguserpassword',
+    }
+
+    const response = await supertest(app.server).post('/user').send(user)
 
     expect(response.status).toBe(400)
   })
 
-  it('should return 400 if the name is missing', async () => {
-    const response = await supertest(app.server).get('/user')
+  it('should return 200 if the name and password contains valid characters', async () => {
+    const user = {
+      name: 'Ricardo',
+      password: '12345678',
+    }
+    const response = await supertest(app.server).post('/user').send(user)
+
+    expect(response.status).toBe(200)
+  })
+
+  it('should return 400 if name and password are missing', async () => {
+    const response = await supertest(app.server).post('/user')
 
     expect(response.status).toBe(400)
   })
